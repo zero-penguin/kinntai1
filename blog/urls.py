62b03810c4b4a7deaf306436850918db334bc649
zero-penguin <arato1060@outlook.jp>
2023-04-07 21:24:13 +0900

    post/new の追加

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 """
 path関数とblogアプリのすべてのインポート
 """
-from django.urls import path
+from django.urls import path 
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
